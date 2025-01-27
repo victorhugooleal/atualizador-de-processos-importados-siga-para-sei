1a5d2e8b99a86fed601e01bd25be8dd4bbdf730f
Victor Leal <victorhugooliveiraleal@gmail.com>
2025-01-27 17:24:17 -0300

    Adiciona suporte a variáveis de ambiente e conexão com o banco de dados MySQL

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
+require('dotenv').config();
+const mysql = require('mysql');
 const fs = require('fs');
 const path = require('path');
 const csv = require('csv-parser');
 
 const csvFilePath = path.join(__dirname, 'data.csv');
 
-const csvData = [];
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err.stack);
+    return;
+  }
+  console.log('Conectado ao banco de dados como id ' + connection.threadId);
+});
 
 fs.createReadStream(csvFilePath)
   .pipe(csv({ separator: ';' }))
   .on('data', (row) => {
-    console.log(`Número Processo SEI: ${row.numero_processo_sei}, Número Processo SIGA: ${row.numero_processo_siga}`);
+    const query = `CALL inserir_migracao('${row.numero_processo_siga}', '${row.numero_processo_sei}')`;
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error('Erro ao executar a procedure:', error);
+        return;
+      }
+      console.log('Procedure executada com sucesso:', results);
+    });
   })
   .on('end', () => {
     console.log('CSV processado com sucesso');
+    connection.query('SHOW ERRORS', (error, results) => {
+      if (error) {
+        console.error('Erro ao mostrar erros:', error);
+        return;
+      }
+      console.log('Erros:', results);
+    });
   });
