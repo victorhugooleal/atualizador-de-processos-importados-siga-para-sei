079081d6379333ced68c87611c04f7fff4555ba6
victor leal <victorhugooliveiraleal@gmail.com>
2025-02-17 11:07:38 -0300

    Atualiza a URL da API para segundas-feiras e melhora a verificação da estrutura da resposta

--- monta_arquivo_data.js
+++ monta_arquivo_data.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const csvFilePath = path.join(__dirname, 'data.csv');
 const urlDefault = 'https://bi.jfes.jus.br/api/public/dashboard/be1387d7-2cd2-41ec-b94a-0be4a9e3e0f0/dashcard/235/card/294?parameters=%5B%7B%22type%22%3A%22string%2F%3D%22%2C%22value%22%3A%5B%22SJRJ%22%2C%22TRF2%22%5D%2C%22id%22%3A%226ccde266%22%2C%22target%22%3A%5B%22dimension%22%2C%5B%22field%22%2C23627%2C%7B%22base-type%22%3A%22type%2FText%22%7D%5D%5D%7D%2C%7B%22type%22%3A%22string%2Fcontains%22%2C%22value%22%3Anull%2C%22id%22%3A%222ed7a42b%22%2C%22options%22%3A%7B%22case-sensitive%22%3Afalse%7D%2C%22target%22%3A%5B%22dimension%22%2C%5B%22field%22%2C23626%2C%7B%22base-type%22%3A%22type%2FText%22%7D%5D%5D%7D%2C%7B%22type%22%3A%22string%2F%3D%22%2C%22value%22%3Anull%2C%22id%22%3A%225bddbaef%22%2C%22target%22%3A%5B%22dimension%22%2C%5B%22field%22%2C23632%2C%7B%22base-type%22%3A%22type%2FText%22%7D%5D%5D%7D%2C%7B%22type%22%3A%22date%2Fall-options%22%2C%22value%22%3A%22past1days%22%2C%22id%22%3A%221a4a3738%22%2C%22target%22%3A%5B%22dimension%22%2C%5B%22field%22%2C23624%2C%7B%22base-type%22%3A%22type%2FDateTime%22%7D%5D%5D%7D%5D';
-const urlMonday = 'https://bi.jfes.jus.br/public/dashboard/be1387d7-2cd2-41ec-b94a-0be4a9e3e0f0?%25C3%25B3rg%25C3%25A3o=SJRJ&%25C3%25B3rg%25A3o=TRF2&data=past3days&tab=60-processos-migrados&tipo_de_processo=&unidade=';
+const urlMonday = 'https://bi.jfes.jus.br/api/public/dashboard/be1387d7-2cd2-41ec-b94a-0be4a9e3e0f0/dashcard/235/card/294?parameters=%5B%7B%22type%22%3A%22string%2F%3D%22%2C%22value%22%3A%5B%22SJRJ%22%5D%2C%22id%22%3A%226ccde266%22%2C%22target%22%3A%5B%22dimension%22%2C%5B%22field%22%2C23627%2C%7B%22base-type%22%3A%22type%2FText%22%7D%5D%5D%7D%2C%7B%22type%22%3A%22string%2Fcontains%22%2C%22value%22%3Anull%2C%22id%22%3A%222ed7a42b%22%2C%22options%22%3A%7B%22case-sensitive%22%3Afalse%7D%2C%22target%22%3A%5B%22dimension%22%2C%5B%22field%22%2C23626%2C%7B%22base-type%22%3A%22type%2FText%22%7D%5D%5D%7D%2C%7B%22type%22%3A%22string%2F%3D%22%2C%22value%22%3Anull%2C%22id%22%3A%225bddbaef%22%2C%22target%22%3A%5B%22dimension%22%2C%5B%22field%22%2C23632%2C%7B%22base-type%22%3A%22type%2FText%22%7D%5D%5D%7D%2C%7B%22type%22%3A%22date%2Fall-options%22%2C%22value%22%3A%22past3days%22%2C%22id%22%3A%221a4a3738%22%2C%22target%22%3A%5B%22dimension%22%2C%5B%22field%22%2C23624%2C%7B%22base-type%22%3A%22type%2FDateTime%22%7D%5D%5D%7D%5D';
 
 const today = new Date();
 const url = today.getDay() === 1 ? urlMonday : urlDefault;
@@ -13,7 +13,12 @@ const url = today.getDay() === 1 ? urlMonday : urlDefault;
 async function fetchData() {
   try {
     const response = await axios.get(url);
-    return response.data.data.rows;
+    console.log('Response data:', response.data); // Log the response data
+    if (response.data && response.data.data && response.data.data.rows) {
+      return response.data.data.rows;
+    } else {
+      throw new Error('Unexpected response structure');
+    }
   } catch (error) {
     console.error('Erro ao buscar dados:', error);
     throw error;
